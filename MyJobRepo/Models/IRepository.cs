d22eb0085ebf2fea47578d0550198e1aeed2a4e4
David Sheean <sheeand@hotmail.com>
2019-04-16 15:53:51 -0400

    Built out repository

--- MyJobRepo/Models/IRepository.cs
+++ MyJobRepo/Models/IRepository.cs
@@ -6,5 +6,15 @@ namespace MyJobRepo.Models
     {
         IQueryable<ContactType> GetAllContactTypes();
         ContactType GetContactTypeById(int id);
+        IQueryable<Contact> GetAllContacts();
+        Contact GetContactById(int id);
+        IQueryable<Company> GetAllCompanies();
+        Company GetCompanyById(int id);
+        IQueryable<Posting> GetAllPostings();
+        Posting GetPostingById(int id);
+        IQueryable<PostingContact> GetAllPostingContacts();
+        PostingContact GetPostingContactById(int id);
+        IQueryable<Event> GetAllEvents();
+        Event GetEventsById(int id);
     }
 }
--- MyJobRepo/Models/Repository.cs
+++ MyJobRepo/Models/Repository.cs
@@ -14,6 +14,8 @@ namespace MyJobRepo.Models
             this.db = db;
         }
 
+        // End points 
+
         public IQueryable<ContactType> GetAllContactTypes()
         {
             return db.ContactTypes;
@@ -22,6 +24,61 @@ namespace MyJobRepo.Models
         public ContactType GetContactTypeById(int id)
         {
             return db.ContactTypes.Find(id);
+        }
+
+
+        public IQueryable<Contact> GetAllContacts()
+        {
+            return db.Contacts;
+        }
+
+        public Contact GetContactById(int id)
+        {
+            return db.Contacts.Find(id);
+        }
+
+
+        public IQueryable<Company> GetAllCompanies()
+        {
+            return db.Companies;
+        }
+
+        public Company GetCompanyById(int id)
+        {
+            return db.Companies.Find(id);
+        }
+
+
+        public IQueryable<Posting> GetAllPostings()
+        {
+            return db.Postings;
+        }
+
+        public Posting GetPostingById(int id)
+        {
+            return db.Postings.Find(id);
+        }
+
+
+        public IQueryable<PostingContact> GetAllPostingContacts()
+        {
+            return db.PostingContacts;
+        }
+
+        public PostingContact GetPostingContactById(int id)
+        {
+            return db.PostingContacts.Find(id);
+        }
+
+
+        public IQueryable<Event> GetAllEvents()
+        {
+            return db.Events;
+        }
+
+        public Event GetEventsById(int id)
+        {
+            return db.Events.Find(id);
         }
 
     }
