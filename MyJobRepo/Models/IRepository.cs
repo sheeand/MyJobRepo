6b5caf881dea88574237297b1333b11c4d2bb55a
David Sheean <sheeand@hotmail.com>
2019-04-17 07:03:52 -0400

    Breeze implementation

--- MyJobRepo/Models/IRepository.cs
+++ MyJobRepo/Models/IRepository.cs
@@ -1,20 +1,36 @@
 ﻿using System.Linq;
+using Breeze.ContextProvider;
+using Newtonsoft.Json.Linq;
 
 namespace MyJobRepo.Models
 {
     public interface IRepository
     {
-        IQueryable<ContactType> GetAllContactTypes();
-        ContactType GetContactType(int id);
-        IQueryable<Contact> GetAllContacts();
-        Contact GetContact(int id);
-        IQueryable<Company> GetAllCompanies();
-        Company GetCompany(int id);
-        IQueryable<Posting> GetAllPostings();
-        Posting GetPosting(int id);
-        IQueryable<PostingContact> GetAllPostingContacts();
-        PostingContact GetPostingContact(int id);
-        IQueryable<Event> GetAllEvents();
-        Event GetEvents(int id);
+        //IQueryable<ContactType> GetAllContactTypes();
+        //ContactType GetContactType(int id);
+        //IQueryable<Contact> GetAllContacts();
+        //Contact GetContact(int id);
+        //IQueryable<Company> GetAllCompanies();
+        //Company GetCompany(int id);
+        //IQueryable<Posting> GetAllPostings();
+        //Posting GetPosting(int id);
+        //IQueryable<PostingContact> GetAllPostingContacts();
+        //PostingContact GetPostingContact(int id);
+        //IQueryable<Event> GetAllEvents();
+        //Event GetEvents(int id);
+
+        string MetaData { get; }
+
+        // One endpoint for all CRUD
+        // jObject is a Json object
+        SaveResult SaveChanges(JObject saveBundle);
+
+        IQueryable<ContactType> ContactTypes();
+        IQueryable<Contact> Contacts();
+        IQueryable<Company> Companies();
+        IQueryable<Posting> Postings();
+        IQueryable<PostingContact> PostingContacts();
+        IQueryable<Event> Events();
+
     }
 }
